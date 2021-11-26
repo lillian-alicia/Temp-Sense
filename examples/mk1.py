746c0da3be72b725345e290bbb08dfb0f1ac398d
R-J Samme <destinydragon890@gmail.com>
2021-11-26 22:27:01 +0000

    main

--- examples/mk1.py
+++ examples/mk1.py
@@ -9,7 +9,7 @@ disp = ST7789.ST7789(
         dc=9,
         backlight=19,               # 18 for back BG slot, 19 for front BG slot.
         spi_speed_hz=80 * 1000 * 1000,
-        offset_left=0 
+        offset_left=0, 
         offset_top = 0
     )
 
@@ -18,3 +18,10 @@ disp.begin()
 WIDTH = disp.width
 HEIGHT = disp.height
 
+img = Image.new('RGB', (WIDTH, HEIGHT), color=(255, 0, 0))
+
+draw = ImageDraw.Draw(img)
+
+draw.rectangle((10, 10, WIDTH - 10, HEIGHT - 10), outline=(255, 255, 0), fill=(255, 0, 255))
+
+disp.display(img)
