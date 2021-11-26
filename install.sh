752c7965e201a3d46c3be00a807bfa08996ad884
R-J Samme <destinydragon890@gmail.com>
2021-11-26 21:01:59 +0000

    Added install script

---
+++ install.sh
@@ -0,0 +1,9 @@
+sudo apt-get install git python3-pip -y
+
+curl https://get.pimoroni.com/bme680 | bash
+
+sudo apt update
+sudo apt install python3-rpi.gpio python3-spidev python3-pip python3-pil python3-numpy
+
+sudo pip3 install st7789
+git clone https://github.com/pimoroni/st7789-python
